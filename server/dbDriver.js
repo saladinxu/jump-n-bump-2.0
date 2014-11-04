5a14257d9acdfab31acb301d4b7abe800577c3c3
saladinxu <saladinxu@gmail.com>
2014-11-05 07:57:01 +0800

    production configs

--- server/dbDriver.js
+++ server/dbDriver.js
@@ -7,9 +7,10 @@ var db;
 
 module.exports = {
     initialize : function(env) {
-        var conf = config[env]||config.dev;
+        var conf = config[env]||config.development;
         var dburl = 'mongodb://' + conf.username + ':' + conf.password + '@' + conf.host +':' + conf.port+ '/' + conf.dbname;
         db = mongo.db(dburl, { native_parser: true });
+        console.log('connected to '+dburl);
     },
     getDb : function() {
         return db;
